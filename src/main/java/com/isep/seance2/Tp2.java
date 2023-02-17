cd448b30c26749795c973764ff5c2261afbdfe76
Annabelle <salinannabelle@gmail.com>
2023-02-17 11:17:14 +0100

    deuxième méthode Fonction is BinarySyminArray(int[]integers, in val)
    Retourne un boolean true si val est la somme de deux entiers du tableau

--- src/main/java/com/isep/seance2/Tp2.java
+++ src/main/java/com/isep/seance2/Tp2.java
@@ -22,7 +22,7 @@ public class Tp2 {
         static boolean isBinarySumInArray(int []integers, int val){
             int n = integers.length;
             int count = 0;
-
+//méthode barbare/Brute force (faire marcher le pb) : complécité 0(n^2)
             for(int i=0; i<n; i++){
                 int subarraySum = 0;
                 for(int j=i; j<n; j++){
@@ -35,15 +35,24 @@ public class Tp2 {
             return count >= 1;
         }
 
+        //méthode par complémentaire (plus efficace que la brute force)
+        public boolean isBinarySumInArray(int[]integers, int val){
+            Set<Integer> set=new HashSet<>();
+            for (int i=0; i<integers.length; i++){
+                if(set.contains(val-integers[i])){
+                    return true;
+                }
+                set.add(integers[]);
+            }
+            return false;
+        }
+
         public static void main(String[] args){
             int [] integers = {3, 5, 7, 15};
             int val = 8;
 
             boolean result = isBinarySumInArray(integers, val);
             System.out.printf("%d est la somme de deux entiers dans le tableau : %b%n", val, result);
-
-
-
 
         }
     }
